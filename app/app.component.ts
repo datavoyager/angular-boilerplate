604192db048a4b2db163ed50dae0148f4ec35e27
l.osullivan <l.osullivan@swansea.ac.uk>
2016-12-02 13:47:36 +0000

    Add binding examples

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  styles: ['input[type="submit"]:first-of-type {color: blue}']
 })
 export class AppComponent {
-  title = "My Application";
- }
+  boundInputValue: string = "Click me!";
+  contentsHeader: string = "Application Contents";
+  components: string[] = [
+    "app.component.html",
+    "app.component.ts",
+    "app.module.ts",
+    "main.ts"
+  ];
+  title: string = "My Application";
+  twoWayBinding: string = "";
+  
+  getContentsHeader(): string {
+    return this.contentsHeader;
+  }
+  useCustomSubmitButton(): boolean {
+    return true;
+  }
+  help(): string {
+    return 'Help label';
+  }
+}
